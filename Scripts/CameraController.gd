db79dd0a860a2b192156056e88844c9746e75aa3
Cassie Smith <cassieistrans@gmail.com>
2025-08-25 12:17:38 -0400

    removed unnecessary graphics

--- Scripts/CameraController.gd
+++
@@ -1,9 +0,0 @@
-extends Camera2D
-
-var pos = Vector2.ZERO
-
-func _physics_process(_delta):
-	position = pos
-	
-func cam_pos(position):
-	pos = position
--- Scripts/label.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-func _ready():
-	var dynamic_font = DynamicFont.new()
-	dynamic_font.font_data = load("res://font.ttf")
-	dynamic_font.size = 10
-	self.set("custom_fonts/font", dynamic_font)
